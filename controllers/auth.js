4465b9a4b27984e9f1d847279f1876d5bd1b9495
Karim Sayed <karimsayed156@outlook.com>
2025-05-01 15:58:18 +0300

    finishing

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,14 @@ const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
-
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        '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'
+    }
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -146,12 +153,8 @@ exports.postSignup = (req, res, next) => {
     })
     .then(result => {
       res.redirect('/login');
-      // return transporter.sendMail({
-      //   to: email,
-      //   from: 'shop@node-complete.com',
-      //   subject: 'Signup succeeded!',
-      //   html: '<h1>You successfully signed up!</h1>'
-      // });
+
+      
     })
     .catch(err => {
       const error = new Error(err);
