538be8c7b4391f30e28f4b774f5bb98eb8df1fd1
Karim Sayed <karimsayed156@outlook.com>
2025-05-01 15:54:10 +0300

    E-Commerce full stack

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,14 +7,7 @@ const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
-const transporter = nodemailer.createTransport(
-  sendgridTransport({
-    auth: {
-      api_key:
-        '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'
-    }
-  })
-);
+
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
